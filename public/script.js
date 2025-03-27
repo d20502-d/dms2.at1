75836c860fe30b637f74fe1d0198ab73a6f9587f
d20502-d <s4000050@student.rmit.edu.au>
2025-03-27 17:06:50 +1100

    1

--- public/script.js
+++ public/script.js
@@ -1,25 +1,12 @@
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+<!doctype html>
+<head>
+    <title> Icon Design </title>
+  
+        }
 
-const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+</head>
 
-const ctx = cnv.getContext (`2d`)
+<body>
 
-const draw_frame = ms => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
-
-   const seconds = (ms / 1000)
-   console.log (seconds.toFixed (2))
-
-   requestAnimationFrame (draw_frame)
-}
-
-draw_frame ()
-
-onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+   
+</script>
